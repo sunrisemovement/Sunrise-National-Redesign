273e9fa253d7b588a636625ded92ef6c3fa8ea6a
Evan Mceldowney <evanmceldowney@gmail.com>
2020-08-10 20:45:28 -0700

    cleaning up styles

--- template-parts/headers/custom-post-header.php
+++ template-parts/headers/custom-post-header.php
@@ -13,37 +13,39 @@
 		<div class="container">
 			<div class="row header-row">
 				<div class="col-md-6 header-blocks header-content">
-					<?php
-			if ( function_exists('yoast_breadcrumb') ) {
-			  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-			}
-			?>
-		<?php if(get_field('secondary_header')): ?>
-			<h4 class="h1-subhead">
-				<?php echo get_field('secondary_header'); ?>
-			</h4>
-		<?php endif?>
-		<?php the_title( '<h1 class="entry-title h2">', '</h1>' ); ?>
-		<?php if(get_field('dates')): ?>
-			<h3 class="dates">
-				<?php echo get_field('dates'); ?> @ <?php echo get_field('time'); ?>
-			</h3>
-		<?php endif?>
-		<?php if(get_field('button_url')): ?>
-			<a href="<?php echo get_field('button_url'); ?>"><button class="header-button yellow">
-			<?php echo get_field('button_text'); ?>
-		</button></a>
-			<?php endif?>
-	</div>
-	<div class="col-md-6 header-blocks header-media">
-		<?php if(get_field('header_embed')): ?>
-			<iframe class="video" width="560" height="315" align="bottom" src="<?php echo get_field('header_embed'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		<?php elseif(get_field('header_image')): ?>
-		<img src="<?php echo get_field('header_image'); ?>" />
-	<?php endif?>
-	</div>
-
-	</div>
-	</div>
-	<?php sunrise_national_post_thumbnail(); ?>
+					<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );}?>
+					<?php if(get_field('secondary_header')): ?>
+						<h4 class="h1-subhead">
+							<?php echo get_field('secondary_header'); ?>
+						</h4>
+					<?php endif?>
+					<?php the_title( '<h1 class="entry-title h2">', '</h1>' ); ?>
+					<?php if(get_field('dates')): ?>
+						<h3 class="dates">
+							<?php echo get_field('dates'); ?> @ <?php echo get_field('time'); ?>
+					</h3>
+				<?php endif?>
+				<?php if(get_field('button_url')): ?>
+					<a href="<?php echo get_field('button_url'); ?>"><button class="header-button yellow">
+						<?php echo get_field('button_text'); ?>
+					</button></a>
+					<?php endif?>
+					</div>
+					<div class="col-md-6 header-blocks header-media">
+						<?php if(get_field('header_embed')): ?>
+							<iframe class="video" width="560" height="315" align="bottom" src="<?php echo get_field('header_embed'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<?php elseif(get_the_post_thumbnail()): ?>
+							<div class="post-thumbnail">
+								<?php the_post_thumbnail('medium'); ?>
+							</div><!-- .post-thumbnail -->
+							<?php endif?>
+						</div>
+					</div>
+			</div>
+			<div class="header-background-image">
+				<?php if(get_field('header_image')): ?>
+					<div class="header-background-image">
+						<img src="<?php echo get_field('header_image'); ?>" />
+					<?php endif?>
+				</div>
 </header><!-- .entry-header -->
